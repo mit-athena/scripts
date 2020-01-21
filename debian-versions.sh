7bbc51d8a4fb04847f80b8c63336ce9f9434f64f
Alexander Chernyakhovsky <achernya@mit.edu>
2020-01-20 19:09:50 -0500

    Update DEBIAN_CODES as well

--- debian-versions.sh
+++ debian-versions.sh
@@ -4,7 +4,7 @@
 # dabuildsys/config.py in build-system.git (checked out on the builder
 # at ~/build-system).
 
-DEBIAN_CODES="wheezy jessie stretch precise trusty xenial artful bionic"
+DEBIAN_CODES="stretch buster xenial bionic"
 gettag() {
     case "$1" in
 	squeeze)
